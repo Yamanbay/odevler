e707e25e607ddf6e4b2b6707e3b0b436c29ec120
Yamanbay <yamanaslibay14@gmail.com>
2025-12-06 15:50:03 +0300

    odev4

---
+++ odev4.java
@@ -0,0 +1,10 @@
+void main(String[] args) {
+    double not1=80;
+    double not2=70;
+    double sonuc=(not1*0.4)+(not2*0.6);
+    if (sonuc>=75){
+        System.out.println("geçtin");
+    }else {
+        System.out.println("kaldın");
+    }
+}
