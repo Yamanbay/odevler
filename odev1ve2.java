cf1230c7eb0be474d1ecf56fa21b1ca1162d8849
Yamanbay <yamanaslibay14@gmail.com>
2025-12-06 15:30:45 +0300

    odev1ve2

---
+++ odev1ve2.java
@@ -0,0 +1,26 @@
+void main(String[] args) {
+    int sonuc;
+    int sayi1=10;
+    int sayi2=17;
+    sonuc=sayi1+sayi2;
+    System.out.println(sonuc);
+    int ciksonuc;
+    int sayi3=28;
+    int sayi4=5;
+    ciksonuc=sayi3-sayi4;
+    System.out.println(ciksonuc);
+    int carpsonuc;
+    carpsonuc=sayi1*sayi2;
+    System.out.println(carpsonuc);
+    int bolsonuc;
+    bolsonuc=sayi1/sayi4;
+    System.out.println(bolsonuc);
+    int cevre;
+    cevre=sayi1*3;
+    System.out.println(cevre);
+    int alan;
+    int t=8;
+    int y=5;
+    alan=8*5/2;
+    System.out.println(alan);
+}
