3558c822eddbff9ba0baa2d6298cc08b58ad1eb1
Yamanbay <yamanaslibay14@gmail.com>
2025-12-06 17:00:21 +0300

    odev3metodlar

---
+++ odev3metodlar.java
@@ -0,0 +1,8 @@
+void main() {
+    String parola="123456";
+    if (parola.length() >= 7) {
+        System.out.println("Şifrenin uzunluğu iyi");
+    } else {
+        System.out.println("Şifren kısa, bunu kabul edemiyoruz");
+    }
+}
